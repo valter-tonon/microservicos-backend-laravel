970340a0c1660dfef38f3845bdf5f0d7c023380c
valter-tonon <tononvalter@gmail.com>
2020-11-19 19:36:09 +0000

    feat/criando crud de categorias e generos

---
+++ app/Http/Controllers/Api/GeneroController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Genero;
+use Illuminate\Http\Request;
+
+class GeneroController extends Controller
+{
+    protected $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Genero::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+        return Genero::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Genero  $genero
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Genero $genero)
+    {
+        return $genero;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Genero  $genero
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Genero $genero)
+    {
+        $this->validate($request, $this->rules);
+        return $genero->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Genero  $genero
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Genero $genero)
+    {
+        $genero->delete();
+        return response()->noContent();
+    }
+}
